2160706687199581e1c8c36bbf86d07843183947
gabriel desharnais <gabriel.desharnais@hotmail.com>
2017-02-27 12:56:21 -0500

    Creating object to be returned by downloader

---
+++ modisSuite/imageObject.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# coding: utf8 
+
+class imageModis:
+    def __init__(files,*arg,**args):
+        self.files=files
+class Afile:
+    def __init__(name,date,typ,*arg,**args):
+        self.name=name
+        self.date=date
+        self.typ=typ
